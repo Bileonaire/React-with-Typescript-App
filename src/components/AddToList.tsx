6642a512de37c378be3adc2f60db11d61c87f820
Bileonaire <kiokoleon@gmail.com>
2022-01-17 11:47:11 +0300

    Add Components

---
+++ src/components/AddToList.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import { IState as Props } from "../App";
+
+interface IProps {
+    setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+    people: Props["people"]
+}
+
+const AddToList: React.FC<IProps> = ({ setPeople, people }) => {
+    const blankForm = {name: "", age: "", note: "", img: ""}
+
+    const [input, setInput] = useState(blankForm)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = () => {
+        if (!input.name || !input.age) return
+
+        setPeople([
+            ...people,
+            {
+                name: input.name,
+                age: parseInt(input.age),
+                img: input.img,
+                note: input.note
+            }
+        ]);
+
+        setInput(blankForm);
+    }
+
+    return (
+        <div className="AddToList">
+            <input
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="name"
+                value={input.name}
+                placeholder="Name"
+            />
+            <input
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="age"
+                value={input.age}
+                placeholder="Age"
+            />
+            <input
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="img"
+                value={input.img}
+                placeholder="Image Url"
+            />
+            <textarea
+                onChange={handleChange}
+                className="AddToList-input"
+                name="note"
+                value={input.note}
+                placeholder="Note"
+            />
+            <button
+                onClick={handleClick}
+                className="AddToList-btn"
+            >
+                Add to List
+            </button>
+        </div>
+    )
+}
+
+export default AddToList
